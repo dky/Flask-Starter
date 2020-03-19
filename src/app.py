b894acb041e9c57319b699c501bf8333d63b0e6d
Don Ky <don.d.ky@gmail.com>
2020-03-18 22:20:40 -0400

    update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+# This means from library import class
 
+# __name__ references app.py
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_whale():
-    return 'Whale, Hello there!'
+@app.route('/', defaults={'name': 'Default'})
+@app.route('/<name>')
+def index(name):
+    return '<h1>Hello {}!</h1>'.format(name)
+
+
+@app.route('/home', methods=['POST'])
+def home():
+    return '<h1>You are on the home page</h1>'
+
+
+@app.route('/json')
+def json():
+    return jsonify({'key': 'value1', 'key2': [1, 2, 3, 4]})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
