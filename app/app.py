15b0e385e1a98462764242a084777e56da77aafc
Don Ky <don.d.ky@gmail.com>
2020-07-07 00:33:11 -0400

    Render first template

--- app/app.py
+++ app/app.py
@@ -4,9 +4,17 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version':  '1.2.3'},
+        {'name': 'sql-flask', 'version':  '2.3.4'},
+        {'name': 'password-crack', 'version':  '5.4.4'},
+    ]
+
+
 @app.route('/')
 def index():
-    test_packages = ['test1', 'test2', 'test3']
+    test_packages = get_latest_packages()
     return flask.render_template('index.html', packages=test_packages)
 
 
